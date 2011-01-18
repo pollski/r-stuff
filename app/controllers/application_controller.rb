844a9aaed76db2721750374875309e88c9c9b23b
erhart <erhart@erharts-MacBook.local>
2011-01-18 12:32:15 -0800

    r-stuff first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < ActionController::Base
+  puts "Hello world!"
+  protect_from_forgery
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RStuff::Application.config.secret_token = 'acad99822cf2ee9229928569f1de3ec18dea0d8e4432d689e98dbaf9d6c55eefed5f5c29ab15e6856630b39faca5c65a4851ba9caa7848c12676d6af26396fdf'
